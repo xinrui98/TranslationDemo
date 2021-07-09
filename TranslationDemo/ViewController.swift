fe462d8385575890bc3d11d8b7db314221bb514f
xinrui98 <gao.xinrui98@gmail.com>
2021-07-09 10:48:27 +0800

    Initial Commit

---
+++ TranslationDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TranslationDemo
+//
+//  Created by Xinrui gao on 9/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
